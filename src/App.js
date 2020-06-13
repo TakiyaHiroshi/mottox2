ceb15ae95f5708370675d72074f481cf04144a60
TakiyaHiroshi <hiroshit@iris.dti.ne.jp>
2020-06-14 06:49:36 +0900

    react-development-beginning_1.5

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React from "react";
+import "minireset.css";
 import "./styles.css";
+import { Helmet } from "react-helmet";
 
-const App = () => {
+export default function App() {
 	return (
 		<div className="App">
-			<header>
-				<h1>Hello CodeSandbox</h1>
-				<nav>
-					<a href="#about">About</a>
-					<a href="#works">Works</a>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>mottox2 ポートフォリオ</title>
+				<meta name="description" content="ポートフォリオサイトのサンプルです" />
+			</Helmet>
+
+			<header className="header">
+				<h1 className="header-title">Portfolio</h1>
+				<nav className="headerNav">
+					<a className="headerNav-item" href="#about">
+						About
+					</a>
+					<a className="headerNav-item" href="#works">
+						Works
+					</a>
 				</nav>
 			</header>
-			<div>
-				<h1>mottox2</h1>
-				<p>Web Engineer, Prototyper</p>
+			<div className="hero">
+				<h1 className="hero-title">mottox2</h1>
+				<p className="hero-description">Web Engineer, Prototyper</p>
 			</div>
-			<section>
-				<h2>About</h2>
-				<p>
+			<section className="section" id="about">
+				<h2 className="section-title">About</h2>
+				<p className="aboutDescription">
 					React、Gatsby、Ruby on Railsを中心に都内で開発しているエンジニアです。
 				</p>
 			</section>
-			<section>
-				<h2>Works</h2>
+			<section className="section" id="works">
+				<h2 className="section-title">Works</h2>
 				<ul>
-					<li>
-						<img src="" />
-						<h3>成果物1</h3>
+					<li className="workItem">
+						<img src="" alt="" />
+						<h3 className="workItem-category">成果物1</h3>
+						<h3 className="workItem-title">すごいC向けサービス</h3>
 					</li>
-					<li>
-						<img src="" />
-						<h3>成果物2</h3>
+					<li className="workItem">
+						<img src="" alt="" />
+						<h3 className="workItem-category">成果物1</h3>
+						<h3 className="workItem-title">すごいC向けサービス</h3>
 					</li>
 				</ul>
 			</section>
-			<footer>
+			<footer className="footer">
 				<a href="https://twitter.com/mottox2">Twitter</a>
 			</footer>
 		</div>
 	);
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	rootElement
 );
